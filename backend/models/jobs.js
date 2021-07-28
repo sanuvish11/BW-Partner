1a26d7c7634848c1db659fe2146d8dce8f778581
sanuvish11 <sanuvish11@gmail.com>
2021-07-28 12:23:39 +0530

    change

--- backend/models/jobs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const T_JobSchema = mongoose.Schema({
-    JOB_NAME: { type: String},
-    STATUS: { type: Number},
-  }, {collection: 'T_Job'});
-  
-  
-  module.exports = mongoose.model("T_Job", T_JobSchema);
